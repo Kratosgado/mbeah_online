e6698a0d87cf0a22edc95a2ad324c6b67456fcd9
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2023-10-24 14:11:05 +0000

    minor

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -7,35 +7,183 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Mbeah's Portfolio"),
+        title: const Text('My Portfolio'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const Text(
-              'Welcome to My Portfolio',
+              'Welcome to Mbeah Online',
               style: TextStyle(
                 fontSize: 24,
                 fontWeight: FontWeight.bold,
               ),
             ),
-            const SizedBox(height: 20),
+            const SizedBox(height: 16),
             const Text(
-              'I am a Flutter Developer',
+              'Your Source for Amazing Web Solutions',
               style: TextStyle(
                 fontSize: 18,
               ),
             ),
-            const SizedBox(height: 40),
+            const SizedBox(height: 32),
             ElevatedButton(
               onPressed: () {
-                // Navigate to the next page or section
+                // Navigation logic to another section or page
               },
-              child: const Text('View My Work'),
+              child: const Text('Explore More'),
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class AboutSection extends StatelessWidget {
+  const AboutSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      child: const Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'About Me',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 16),
+          Text(
+            'I am a passionate Flutter developer with experience in building beautiful and performant mobile and web applications.',
+            style: TextStyle(
+              fontSize: 18,
+            ),
+          ),
+          SizedBox(height: 16),
+          Text(
+            'Skills:',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 8),
+          Text(
+            '• Flutter\n'
+            '• Dart\n'
+            '• Firebase\n'
+            '• HTML/CSS/JavaScript',
+            style: TextStyle(
+              fontSize: 16,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class PortfolioSection extends StatelessWidget {
+  const PortfolioSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      child: const Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Portfolio',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 16),
+          // Add your project cards or work samples here
+        ],
+      ),
+    );
+  }
+}
+
+class ServicesSection extends StatelessWidget {
+  const ServicesSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      child: const Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Services',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 16),
+          // Add your services description here
+        ],
+      ),
+    );
+  }
+}
+
+class TestimonialsSection extends StatelessWidget {
+  const TestimonialsSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      child: const Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Testimonials',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 16),
+          // Add your testimonials or client feedback here
+        ],
+      ),
+    );
+  }
+}
+
+class ContactSection extends StatelessWidget {
+  const ContactSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      child: const Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Contact',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 16),
+          // Add your contact information or contact form here
+        ],
       ),
     );
   }
