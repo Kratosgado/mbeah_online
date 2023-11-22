60e13c100f8fa0ba5e29feba1ce211c216ff41e5
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2023-11-22 08:48:35 +0000

    restart design

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,16 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
-import 'package:mbeah_online/constants.dart';
-import 'package:mbeah_online/landscape_view.dart';
-import 'package:mbeah_online/sections/about.dart';
 
-import 'homepage.dart';
-import 'sections/contacts.dart';
-import 'sections/projects.dart';
-import 'sections/services.dart';
-import 'sections/testimonial.dart';
+
+import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(const ProviderScope(
-    child: MainApp(),
-  ));
+  runApp(const MyPortfolio());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyPortfolio extends StatelessWidget {
+  const MyPortfolio({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: "Mbeah's Portfolio",
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        useMaterial3: true,
-        colorScheme: const ColorScheme.dark(),
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        primaryColor: Colors.indigo,
-        secondaryHeaderColor: Colors.lightBlue,
-      ),
-      routerConfig: router,
-      // routerDelegate: router.routerDelegate,
-      // routeInformationParser: router.routeInformationParser,
-    );
+    return Container();
   }
 }
-
-final rootNavigatorKey = GlobalKey<NavigatorState>();
-final shellNavigatorKey = GlobalKey<NavigatorState>();
-
-final router = GoRouter(
-  navigatorKey: rootNavigatorKey,
-  initialLocation: '/',
-  routes: [
-    ShellRoute(
-      navigatorKey: shellNavigatorKey,
-      builder: (context, state, child) => HomePage(child: child),
-      routes: [
-        GoRoute(
-          path: '/',
-          builder: (context, state) => const LandscapeView(),
-          routes: [
-            buildSection('testimonials', const TestimonialsSection()),
-            buildSection('projects', const ProjectSection(projects: [],)),
-            buildSection('services', const ServicesSection()),
-            buildSection('contacts', const ContactSection()),
-            buildSection('about', const AboutSection())
-          ],
-        )
-      ],
-    ),
-  ],
-);
